1bc9891616d81d565933000034553f186bf9d146
dtiern5 <dannyjtierney@gmail.com>
2021-07-07 07:40:40 -0500

    combobox for divisions in AddCustomer scene working

--- src/Controller/AddCustomerController.java
+++ src/Controller/AddCustomerController.java
@@ -1,6 +1,11 @@
 package Controller;
 
+import DBAccess.DBDivisions;
+import Database.DBConnection;
+import Database.DBQuery;
 import Model.Division;
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,22 +16,45 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AddCustomerController implements Initializable {
 
     @FXML
+    private TextField customerIdTF;
+    @FXML
+    private TextField nameTF;
+    @FXML
+    private TextField addressTF;
+    @FXML
+    private TextField postalCodeTF;
+    @FXML
     private ComboBox<Division> divisionComboBox;
+    @FXML
+    private TextField countryTF;
+    @FXML
+    private TextField phoneNumberTF;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            ObservableList<Division> divisionList = DBDivisions.getAllDivisions();
+            divisionComboBox.setItems(divisionList);
 
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     /**
@@ -46,6 +74,47 @@ public class AddCustomerController implements Initializable {
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
             window.setScene(mainViewScene);
             window.show();
+        }
+    }
+
+    public void confirmHandler(ActionEvent event) {
+        Connection conn = DBConnection.getConnection();
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        try{
+            alert.setTitle("Customer not added");
+
+            if(nameTF.getText().isEmpty() ||
+            addressTF.getText().isEmpty() ||
+            postalCodeTF.getText().isEmpty() ||
+            phoneNumberTF.getText().isEmpty()) {
+                alert.setContentText("All fields require values");
+                alert.showAndWait();
+            }
+
+            String insertStatement = "INSERT INTO customers(Customer_Name, Address, Postal_Code, Phone)" +
+                    "VALUES(?, ?, ?, ?)";
+
+            DBQuery.setPreparedStatement(conn, insertStatement);
+
+            PreparedStatement ps = DBQuery.getPreparedStatement();
+
+            String customerName = nameTF.getText();
+            String address = addressTF.getText();
+            String postalCode = postalCodeTF.getText();
+            String phoneNumber = phoneNumberTF.getText();
+
+            ps.setString(1, customerName);
+            ps.setString(2, address);
+            ps.setString(3, postalCode);
+            ps.setString(4, phoneNumber);
+
+            ps.execute();
+
+        } catch (Exception e) {
+            System.out.println("Input error: " + e.getMessage());
+            alert.setTitle("Input error");
+            alert.setContentText("Placeholder");
         }
     }
 }
--- src/Model/Division.java
+++ src/Model/Division.java
@@ -75,4 +75,10 @@ public class Division {
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
+
+    @Override
+    public String toString() {
+        return (division);
+    }
+
 }
